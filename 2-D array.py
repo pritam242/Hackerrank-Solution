9d0e17112c5539dcdb321c6dea31706bc69db9df
Pritam Das <59303654+pritam242@users.noreply.github.com>
2020-01-15 11:31:42 +0530

    Add files via upload

---
+++ 2-D array.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 15 11:28:53 2020
+
+@author: Pritam
+"""
+
+arr = []
+
+for _ in range(6):
+    tmp = [int(x) for x in str(input()).split(" ")]
+    arr.append(tmp)
+
+maximum = -9 * 7
+
+for i in range(6):
+    for j in range(6):
+        if j + 2 < 6 and i + 2 < 6:
+            result = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2]
+            if result > maximum:
+                maximum = result
+
+print(maximum)
